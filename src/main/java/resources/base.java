7259f05d1804bd8919a19d4bfaec92bea88b24eb
ramonmtz11 <martkint@gmail.com>
2020-04-24 17:07:10 -0500

    Select browser from jenkins

--- src/main/java/resources/base.java
+++ src/main/java/resources/base.java
@@ -30,14 +30,14 @@ public class base {
 		prop = new Properties();
 		FileInputStream fi = new FileInputStream(
 				fullpath);
-		//String browserName = System.getProperty("browser");
+		String browserName = System.getProperty("browser");
 		prop.load(fi);
-		 String browserName = prop.getProperty("browser");
+		 //String browserName = prop.getProperty("browser");
 
 		if (browserName.equals("chrome")) {
 			System.setProperty("webdriver.chrome.driver",
 					"C:\\Users\\699693\\Documents\\Cognizant\\Automation Course\\Automation Course\\src\\Executables\\chromedriver.exe");
-			//ChromeOptions options = new ChromeOptions();
+			ChromeOptions options = new ChromeOptions();
 			//options.addArguments("headless");
 			driver = new ChromeDriver();
 
